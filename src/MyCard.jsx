1b596c2c3623bd82472ee866afd16dcfb4a893f9
nattapatNtp <nattapat.klu@ku.th>
2024-10-13 15:43:42 +0700

    edit 3

--- src/MyCard.jsx
+++ src/MyCard.jsx
@@ -26,8 +26,9 @@ export default function MyCard(props) {
     </Typography>
   </CardContent>
   <CardActions>
-  <Button size="small">xXx</Button>
+    <Button size="small">xXx</Button>
     <Button size="small">Learn More</Button>
+    <Button size="small">Sawasdeekub</Button>
   </CardActions>
 </Card>
 
